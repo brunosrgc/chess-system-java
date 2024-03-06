0bc508477d56dba1862cb091ef0593ade66e71c2
Bruno Sérgio <brunosergiocosta5@gmail.com>
2024-03-06 17:17:52 -0300

    First class: Position

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,11 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Progam {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello word!");
-
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
-
 }
